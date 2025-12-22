b3011f0c8c101adc19442ce09c50caabb621a2e4
Guilherme Saturnino <guilherme.bicalho2@gmail.com>
2025-12-22 11:36:55 -0300

    Day10-part2

--- build.zig
+++ build.zig
@@ -3,7 +3,12 @@ const std = @import("std");
 const Solution = struct { day: []const u8, file: []const u8 };
 
 pub fn build(b: *std.Build) !void {
-    const solutions = [_]Solution{ .{ .day = "hello", .file = "src/hello.zig" }, .{ .day = "day01", .file = "src/day01.zig" }, .{ .day = "day02", .file = "src/day02.zig" } };
+    const solutions = [_]Solution{
+        .{ .day = "hello", .file = "src/hello.zig" },
+        .{ .day = "day01", .file = "src/day01.zig" },
+        .{ .day = "day02", .file = "src/day02.zig" },
+        .{ .day = "day10", .file = "src/day10.zig" },
+    };
     const mvzr = b.dependency("mvzr", .{});
     for (solutions) |sol| {
         const exe = b.addExecutable(.{
@@ -13,8 +18,12 @@ pub fn build(b: *std.Build) !void {
                 .target = b.graph.host,
             }),
         });
-        b.installArtifact(exe);
         exe.root_module.addImport("mvzr", mvzr.module("mvzr"));
+        exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/Cellar/highs/1.12.0/include/highs" });
+        exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/Cellar/highs/1.12.0/lib" });
+        exe.linkSystemLibrary("highs");
+
+        b.installArtifact(exe);
         const run_exe = b.addRunArtifact(exe);
         const run_step = b.step(sol.day, "Run executable");
         run_step.dependOn(&run_exe.step);
--- src/day10.zig
+++ src/day10.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
 const mvzr = @import("mvzr");
+const c = @cImport({
+    @cInclude("interfaces/highs_c_api.h");
+});
+
+// C integer types
+pub const c_double = f64;
+pub const c_ptr = *anyopaque;
+const NULL_CINT_PTR: *c_int = @ptrCast(@constCast(&[_]c_int{0}));
+const NULL_CDOUBLE_PTR: *c_double = @ptrCast(@constCast(&[_]c_double{0}));
 
 const MAX_U8: usize = 1 << 63;
 
@@ -42,6 +51,56 @@ const Puzzle = struct {
         }
         unreachable;
     }
+    fn solve_pt2(self: @This(), allocator: std.mem.Allocator) !usize {
+        const highs = c.Highs_create().?;
+        defer _ = c.Highs_destroy(highs); // Ensure cleanup
+        const inf = c.Highs_getInfinity(highs);
+        const num_cols = self.buttons.len;
+        const num_rows = self.joltage_reqs.len;
+        for (0..num_cols) |i| {
+            if (c.Highs_addCol(
+                highs,
+                1,
+                0.0,
+                inf,
+                0,
+                NULL_CINT_PTR,
+                NULL_CDOUBLE_PTR,
+            ) != c.kHighsStatusOk) {
+                return error.FailedToAddCols;
+            }
+            if (c.Highs_changeColIntegrality(highs, @as(c_int, @intCast(i)), c.kHighsVarTypeInteger) != c.kHighsStatusOk) {
+                return error.FailedToSetIntegrality;
+            }
+        }
+
+        var indices: std.ArrayList(c_int) = .empty;
+        defer indices.deinit(allocator);
+        var values = [_]c_double{1.0} ** 32;
+        var cur_idx: usize = 0;
+        std.debug.print("Matrix:\n", .{});
+        for (0..num_rows) |i| {
+            var num_new_nz: c_int = 0;
+            for (0..num_cols) |j| {
+                if ((self.buttons[j] >> @as(u6, @intCast(i))) % 2 == 1) {
+                    try indices.append(allocator, @as(c_int, @intCast(j)));
+                    std.debug.print("{d}, ", .{j});
+                    num_new_nz += 1;
+                }
+            }
+            std.debug.print("\n", .{});
+            if (c.Highs_addRow(highs, @floatFromInt(self.joltage_reqs[i]), @floatFromInt(self.joltage_reqs[i]), num_new_nz, &indices.items[cur_idx], &values[0]) != c.kHighsStatusOk) {
+                return error.FailedToAddCols;
+            }
+            cur_idx += @intCast(num_new_nz);
+        }
+        if (c.Highs_run(highs) != c.kHighsStatusOk) {
+            return error.HighsRunFailed;
+        }
+
+        const objective_value: c_double = c.Highs_getObjectiveValue(highs);
+        return @intFromFloat(objective_value);
+    }
 };
 
 fn read_puzzle(line: []const u8, allocator: std.mem.Allocator) !Puzzle {
@@ -83,11 +142,14 @@ pub fn main() !void {
     defer allocator.free(content);
     var it = std.mem.splitScalar(u8, content, '\n');
     var sol_pt1: usize = 0;
+    var sol_pt2: usize = 0;
     while (it.next()) |line| {
         if (line.len == 0) continue;
         const puzzle = try read_puzzle(line, allocator);
         defer puzzle.deinit(allocator);
         sol_pt1 += try puzzle.solve_pt1(allocator);
+        sol_pt2 += try puzzle.solve_pt2(allocator);
     }
     std.debug.print("Part 1: {d}\n", .{sol_pt1});
+    std.debug.print("Part 2: {d}\n", .{sol_pt2});
 }
